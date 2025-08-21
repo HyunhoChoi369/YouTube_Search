4ea137ab8b719c3fdd4e0aac94c88057ac113fa3
HYUNHO CHOI <hyunhochoi@HYUNHOui-Macmini.local>
2025-08-21 20:16:50 +0900

    Update project files

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # app.py
-import sys, os
-sys.path.append(os.path.dirname(__file__))  # 현재 폴더를 모듈 경로에 추가
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
 
 import streamlit as st
 from lib import config, providers, utils
